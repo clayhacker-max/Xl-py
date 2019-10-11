4a009e5a5c48986ed7f47d7f0adf0d45c6ac7966
clayhacker-max <56114046+clayhacker-max@users.noreply.github.com>
2019-10-11 13:35:01 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 by Anggi Alberto'
+__version__         = '3.0.0'
+__author__          = 'Mr.Gaming'
+__author_email__    = 'clayhackerteam88@gmail.com'
+__url__             = 'http://github.com/clayhacker-max/xl-py'
+
+__all__ = ['Config','XL']
